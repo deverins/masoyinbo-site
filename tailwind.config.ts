203fe9812f44b9f57e68578c5ece042032b02fdc
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-08-18 09:29:55 +0100

    feat: Added a tailwind config for css color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        skyBlue: '#72C3D5',
+        navyBlue: '#002F5E',
+        saffron: '#F9BC33',
+        terracotta: '#CB6F35',
+        cream: '#F5F5F5',
+        green: '#006B35',
+      },
     },
   },
   plugins: [],
