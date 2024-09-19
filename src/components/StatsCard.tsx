3937642d0eb05719f8f0805543dc15b09183e761
deverins <olayiwolapamilerin34@gmail.com>
2024-09-19 18:19:20 +0100

    feat/ updated StatsCard component

--- src/components/StatsCard.tsx
+++ src/components/StatsCard.tsx
@@ -53,57 +53,51 @@ const StatsCard: React.FC = () => {
       <div className='mt-20 mx-0 lg:mx-20 md:mx-10'>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
           {/* Total Episodes */}
-          <div className="p-4 bg-secondary-cream rounded-lg text-center shadow-md">
+          <div className="p-4 bg-secondary-cream rounded-lg text-center shadow-md py-6">
             <Link href='all-episodes'>
               <h3 className="text-lg font-semibold flex justify-center items-center">
                 Total Episodes
-                <PlayIcon className="h-6 w-6 text-blue-500 mr-2" />
+                <PlayIcon className="h-6 w-6 text-blue-500 " />
               </h3>
               <p className="text-2xl font-bold text-blue-500">{stats.totalEpisodes}</p>
             </Link>
           </div>
 
           {/* Total Amount Won */}
-          <div className="p-4 bg-secondary-cream rounded-lg text-center shadow-md">
-            <Link href='/episode-events-details'>
-              <h3 className="text-lg font-semibold flex justify-center items-center">
-                Total Amount Won
-                <div className="h-6 w-6 bg-green-500 rounded-full mr-2" > <span className='text-white'>&#x20A6;</span></div>
-              </h3>
-              <p className="text-2xl font-bold text-green-500">₦{stats.totalAmountWon.toLocaleString()}</p>
-            </Link>
+          <div className="p-4 bg-secondary-cream rounded-lg text-center shadow-md py-6">
+            <h3 className="text-lg font-semibold flex justify-center items-center">
+              Total Amount Won
+              <div className="h-6 w-6 bg-green-500 rounded-full " > <span className='text-white'>&#x20A6;</span></div>
+            </h3>
+            <p className="text-2xl font-bold text-green-500">₦{stats.totalAmountWon.toLocaleString()}</p>
           </div>
 
           {/* Total Asked Questions */}
-          <div className="p-4 bg-secondary-cream rounded-lg text-center shadow-md">
-            <Link href='/episode-events-details'>
-              <h3 className="text-lg font-semibold flex justify-center items-center">
-                Total Asked Questions
-                <QuestionMarkCircleIcon className="h-6 w-6 text-secondary-saffronLight mr-2" />
-              </h3>
-              <p className="text-2xl font-bold text-secondary-saffronLight">{stats.totalAskedQuestions.count}</p>
-            </Link>
+          <div className="p-4 bg-secondary-cream rounded-lg text-center shadow-md py-6">
+            <h3 className="text-lg font-semibold flex justify-center items-center">
+              Total Asked Questions
+              <QuestionMarkCircleIcon className="h-6 w-6 text-secondary-saffronLight" />
+            </h3>
+            <p className="text-2xl font-bold text-secondary-saffronLight">{stats.totalAskedQuestions.count}</p>
           </div>
 
           {/* Total Right Questions */}
-          <div className="p-4 bg-secondary-cream rounded-lg text-center shadow-md">
-            <Link href='/episode-events-details'>
-              <h3 className="text-lg font-semibold flex justify-center items-center">
-                Total Correct Answer
-                <CheckCircleIcon className="h-6 w-6 text-teal-500 mr-2" />
-              </h3>
-              <p className="text-2xl font-bold text-teal-500">{stats.totalRightQuestions.count}</p>
-            </Link>
+          <div className="p-4 bg-secondary-cream rounded-lg text-center shadow-md py-6">
+            <h3 className="text-lg font-semibold flex justify-center items-center">
+              Total Correct Answer
+              <CheckCircleIcon className="h-6 w-6 text-teal-500 " />
+            </h3>
+            <p className="text-2xl font-bold text-teal-500">{stats.totalRightQuestions.count}</p>
           </div>
         </div>
 
         {/*larger screens centered card*/}
         <div className="flex justify-center">
-          <div className="p-4 bg-secondary-cream rounded-lg text-center shadow-md">
+          <div className="p-4 bg-secondary-cream rounded-lg text-center shadow-md w-96 py-6">
             <Link href='/request-pool'>
               <h3 className="text-lg font-semibold flex justify-center items-center">
                 Participant Request
-                <UserGroupIcon className="h-6 w-6 text-orange-500 mr-2" />
+                <UserGroupIcon className="h-6 w-6 text-orange-500 " />
               </h3>
               <p className="text-2xl font-bold text-orange-500">{stats.requestPool.total.toLocaleString()}</p>
             </Link>
