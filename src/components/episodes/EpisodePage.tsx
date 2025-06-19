2d1f953ba576ef4466ea6630896a30b7557d05b3
deverins <olayiwolapamilerin34@gmail.com>
2025-06-19 19:50:20 +0100

    fix/ Adjust scroll behavior to stop at sticky episode header

--- src/components/episodes/EpisodePage.tsx
+++ src/components/episodes/EpisodePage.tsx
@@ -150,120 +150,185 @@ const EpisodePage: React.FC = () => {
 
   if (loading) return <div className='min-h-screen flex justify-center'><Loading /></div>;
 
-  return (<>
-    {episodeDetails ?
-      <>
-        <div className="flex flex-col items-center py-2">
-          <div className="mx-auto w-full max-w-screen-sm sm:mx-20">
-            <VideoPreview videoLink={episodeDetails.episode.episodeLink} title="" />
+  return (
+    <>
+      {episodeDetails ? (
+        <>
+          <div className="sticky top-[75px] h-auto z-10 bg-slate-200 dark:bg-slate-900">
+            <div className="flex flex-col items-center py-4">
+              <div className="mx-auto w-full max-w-screen-sm sm:mx-20">
+                <VideoPreview
+                  videoLink={episodeDetails.episode.episodeLink}
+                  title=""
+                />
+              </div>
+
+              {/* Episode Details */}
+              <div className="mt-4 w-full max-w-[560px]">
+                <div className="dark:text-neutral-300 font-semibold">
+                  Episode number: {episodeDetails.episode.episodeNumber}
+                </div>
+                <div className="dark:text-neutral-300 font-semibold">
+                  Participant Name: {episodeDetails.participantFullName}
+                </div>
+                <div className="dark:text-neutral-300 font-semibold">
+                  Total Amount Available:{" "}
+                  {formatCurrency(episodeDetails.episode.availableAmountToWin)}
+                </div>
+                <div className="text-md dark:text-neutral-300 font-semibold">
+                  Amount won: {formatCurrency(episodeDetails.episode.amountWon)}
+                </div>
+                <div className="dark:text-neutral-300 font-semibold">
+                  Episode Date: {episodeDetails.episode.episodeDate ?? ""}
+                </div>
+              </div>
+            </div>
           </div>
 
-          {/* Episode Details */}
-          <div className="mt-4 w-full max-w-[560px] mb-10">
-            <div className="dark:text-neutral-300 font-semibold">
-              Episode number: {episodeDetails.episode.episodeNumber}
+          {isAdmin && (
+            <div className="mt-4 w-full">
+              <div className="flex gap-2 mx-4 justify-end mt-4">
+                <button
+                  onClick={handleEditEpisode}
+                  className="bg-blue-500 text-white hover:bg-blue-600 p-2 rounded flex items-center justify-center"
+                >
+                  <PencilIcon className="h-5 w-5" />
+                  Edit Episode
+                </button>
+                <button
+                  onClick={() => setOpenDeleteModal(true)}
+                  className="bg-red-500 text-white rounded hover:bg-red-600 p-2 flex items-center justify-center"
+                >
+                  <TrashIcon className="h-5 w-5" />
+                  Delete Episode
+                </button>
+              </div>
             </div>
-            <div className="dark:text-neutral-300 font-semibold">
-              Participant Name: {episodeDetails.participantFullName}
+          )}
+          <h3 className="text-center font-bold text-xl dark:text-neutral-200 mt-6">
+            Episode Events
+          </h3>
+          {isAdmin && (
+            <div className="flex justify-end pt-4">
+              <button
+                onClick={addEvent}
+                className="bg-primary-light flex text-neutral-200 p-2 rounded-lg mb-2"
+              >
+                <PlusIcon className="h-5 w-5 mr-2" /> Add Event
+              </button>
             </div>
-            <div className="dark:text-neutral-300 font-semibold">
-              Total Amount Available: {formatCurrency(episodeDetails.episode.availableAmountToWin)}
+          )}
+
+          <EventsTable events={episodeDetails.events} signal={signal} />
+
+          {/* Episode Event Modal */}
+          <Modal
+            trigger={openEpisodeEditModal}
+            close={closeEditEpisodeModal}
+            side="center"
+            backgroundColorClass="bg-secondary-cream dark:bg-slate-900"
+          >
+            <div className="w-[calc(100dvw-12px)] max-w-[600px] p-2">
+              <CreateEpisodeForm
+                onSaveEpisode={onSaveEpisode}
+                episodeId={episodeId}
+                editEpisode={selectedEpisode}
+              />
             </div>
-            <div className="text-md dark:text-neutral-300 font-semibold">
-              Amount won: {formatCurrency(episodeDetails.episode.amountWon)}
+          </Modal>
+
+          {/* Create And Edit Event Modal */}
+          <Modal
+            trigger={openEditModal}
+            close={closeEditModal}
+            side="bottom"
+            backgroundColorClass="bg-secondary-cream dark:bg-slate-900"
+          >
+            <div className="w-[calc(100dvw-12px)] max-h-[60dvh] max-w-screen-lg mx-auto p-2">
+              <EventsForm
+                onSave={onSave}
+                event={selectedEvent}
+                episodeId={episodeId}
+              />
             </div>
-            <div className="dark:text-neutral-300 font-semibold">
-              Episode Date: {(episodeDetails.episode.episodeDate ?? '')}
+          </Modal>
+
+          {/* Delete Event Confirmation Modal */}
+          <Modal
+            trigger={openDeleteEventModal}
+            close={() => setOpenDeleteEventModal(false)}
+            side="center"
+            gum
+            backgroundColorClass="bg-secondary-cream dark:bg-slate-900 rounded"
+          >
+            <div className="p-4 w-[calc(100dvw-12px)] max-w-[600px]">
+              <h2 className="text-lg font-bold dark:text-neutral-200">
+                Confirm Delete
+              </h2>
+              <p className="dark:text-neutral-200">
+                Are you sure you want to delete this event? This action cannot
+                be undone.
+              </p>
+              <div className="mt-4 flex justify-end">
+                <button
+                  onClick={closeDeleteModal}
+                  className="mr-2 bg-gray-300 hover:bg-gray-400 p-2 rounded"
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={() => {
+                    if (eventToDelete) handleDelete(eventToDelete);
+                    setOpenDeleteEventModal(false);
+                  }}
+                  className="bg-red-500 hover:bg-red-600 text-white p-2 rounded"
+                >
+                  Delete
+                </button>
+              </div>
             </div>
-          </div>
+          </Modal>
+
+          {/* Delete Confirmation Modal */}
+          <Modal
+            trigger={openDeleteModal}
+            close={() => setOpenDeleteModal(false)}
+            side="center"
+            gum
+            backgroundColorClass="bg-secondary-cream dark:bg-slate-900 rounded"
+          >
+            <div className="p-4 w-[calc(100dvw-12px)] max-w-[600px]">
+              <h2 className="text-lg font-bold dark:text-neutral-200">
+                Confirm Delete
+              </h2>
+              <p className=" dark:text-neutral-200">
+                Are you sure you want to delete this episode? This action cannot
+                be undone.
+              </p>
+              <div className="mt-4 flex justify-end">
+                <button
+                  onClick={() => setOpenDeleteModal(false)}
+                  className="mr-2 bg-gray-300 hover:bg-gray-400 p-2 rounded"
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={handleDeleteEpisode}
+                  className="bg-red-500 hover:bg-red-600 text-white p-2 rounded"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </Modal>
+        </>
+      ) : (
+        <div className="text-red-500 text-center mt-4">
+          {error ? error : "No episode details available."}
         </div>
-        {isAdmin && (
-          <div className="mt-4 w-full">
-            <div className="flex gap-2 mx-4 justify-end mt-4">
-              <button onClick={handleEditEpisode}
-                className="bg-blue-500 text-white hover:bg-blue-600 p-2 rounded flex items-center justify-center">
-                <PencilIcon className="h-5 w-5" />
-                Edit Episode
-              </button>
-              <button
-                onClick={() => setOpenDeleteModal(true)}
-                className="bg-red-500 text-white rounded hover:bg-red-600 p-2 flex items-center justify-center">
-                <TrashIcon className="h-5 w-5" />
-                Delete Episode
-              </button>
-            </div>
-          </div>
-        )}
-        <h3 className="text-center font-bold text-xl dark:text-neutral-200 mt-6">Episode Events</h3>
-        {isAdmin && (
-          <div className='flex justify-end pt-4'>
-            <button onClick={addEvent}
-              className="bg-primary-light flex text-neutral-200 p-2 rounded-lg mb-2">
-              <PlusIcon className="h-5 w-5 mr-2" /> Add Event
-            </button>
-          </div>
-        )}
-
-        <EventsTable events={episodeDetails.events} signal={signal} />
-
-        {/* Episode Event Modal */}
-        <Modal trigger={openEpisodeEditModal} close={closeEditEpisodeModal} side="center"
-          backgroundColorClass="bg-secondary-cream dark:bg-slate-900">
-          <div className="w-[calc(100dvw-12px)] max-w-[600px] p-2">
-            <CreateEpisodeForm onSaveEpisode={onSaveEpisode} episodeId={episodeId} editEpisode={selectedEpisode} />
-
-          </div>
-        </Modal>
-
-        {/* Create And Edit Event Modal */}
-        <Modal trigger={openEditModal} close={closeEditModal} side="bottom" 
-          backgroundColorClass="bg-secondary-cream dark:bg-slate-900">
-          <div className="w-[calc(100dvw-12px)] max-h-[60dvh] max-w-screen-lg mx-auto p-2">
-            <EventsForm onSave={onSave} event={selectedEvent} episodeId={episodeId} />
-          </div>
-        </Modal>
-
-        {/* Delete Event Confirmation Modal */}
-        <Modal
-          trigger={openDeleteEventModal}
-          close={() => setOpenDeleteEventModal(false)}
-          side="center"
-          gum
-          backgroundColorClass="bg-secondary-cream dark:bg-slate-900 rounded">
-          <div className="p-4 w-[calc(100dvw-12px)] max-w-[600px]">
-            <h2 className="text-lg font-bold dark:text-neutral-200">Confirm Delete</h2>
-            <p className="dark:text-neutral-200">Are you sure you want to delete this event? This action cannot be undone.</p>
-            <div className="mt-4 flex justify-end">
-              <button onClick={closeDeleteModal} className="mr-2 bg-gray-300 hover:bg-gray-400 p-2 rounded">Cancel</button>
-              <button
-                onClick={() => {
-                  if (eventToDelete) handleDelete(eventToDelete);
-                  setOpenDeleteEventModal(false);
-                }}
-                className="bg-red-500 hover:bg-red-600 text-white p-2 rounded">Delete</button>
-            </div>
-          </div>
-        </Modal>
-
-        {/* Delete Confirmation Modal */}
-        <Modal trigger={openDeleteModal} close={() => setOpenDeleteModal(false)} side="center" gum
-          backgroundColorClass="bg-secondary-cream dark:bg-slate-900 rounded">
-          <div className="p-4 w-[calc(100dvw-12px)] max-w-[600px]">
-            <h2 className="text-lg font-bold dark:text-neutral-200">Confirm Delete</h2>
-            <p className=" dark:text-neutral-200">Are you sure you want to delete this episode? This action cannot be undone.</p>
-            <div className="mt-4 flex justify-end">
-              <button onClick={() => setOpenDeleteModal(false)} className="mr-2 bg-gray-300 hover:bg-gray-400 p-2 rounded">Cancel</button>
-              <button onClick={handleDeleteEpisode} className="bg-red-500 hover:bg-red-600 text-white p-2 rounded">Delete</button>
-            </div>
-          </div>
-        </Modal>
-      </>
-      :
-      <div className="text-red-500 text-center mt-4">
-        {error ? error : 'No episode details available.'}
-      </div>
-    }
-  </>)
+      )}
+    </>
+  );
 }
 
 export default EpisodePage;
